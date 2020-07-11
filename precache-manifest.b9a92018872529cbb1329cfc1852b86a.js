69e4f52f2cdf01793af5c390072155d5488d6e87
Roman Zabolotskyi <roman.zabolotskyi@gmail.com>
2020-07-11 13:23:49 +0300

    Updates

---
+++ precache-manifest.b9a92018872529cbb1329cfc1852b86a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d507fbf32c17471854d5b32a577ab1cc",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "a3ba54f6c14e0d0baa4c",
+    "url": "/social-network/static/css/main.ce88ef77.chunk.css"
+  },
+  {
+    "revision": "de07d646431259012365",
+    "url": "/social-network/static/js/2.3c6ccfc6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/social-network/static/js/2.3c6ccfc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3ba54f6c14e0d0baa4c",
+    "url": "/social-network/static/js/main.0ee5afb4.chunk.js"
+  },
+  {
+    "revision": "5724ed3390db798d081c",
+    "url": "/social-network/static/js/runtime-main.1d4a647c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/social-network/profile/precache-manifest.1a927541f41c83abe25006d461a06b46.js"
+  "/social-network/precache-manifest.b9a92018872529cbb1329cfc1852b86a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/social-network/profile/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/social-network/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
